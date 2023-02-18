cb19548e97d613072017df656e64e0f0460c477f
Michelle Senar Dressler <senarmichelle@gmail.com>
2023-02-17 16:27:53 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Tagalog Verbs
+A more intense Django project to help memorize and conjugate Tagalog verbs.
+
+## Setup
+
+```code
+python3.10 -m venv ENV
+source ENV/bin/activate
+pip install --upgrade pip setuptools
+pip install -e .
+manage migrate
+manage build_verbs
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+requirements = [
+    "Django",
+    "dj_database_url",
+    "gunicorn",
+    "psycopg2",
+    "python-dotenv",
+    "whitenoise",
+]
+
+setup(
+    name="tagalog-verbs",
+    version="0.1",
+    description="Practice Tagalog Verbs conjugation",
+    url="https://github.com/michellesenar/tagalog-verbs",
+    author="Michelle Senar Dressler",
+    license="MIT",
+    install_requires=requirements,
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    entry_points={
+        "console_scripts": ["manage = manage:main"],
+    },
+)
