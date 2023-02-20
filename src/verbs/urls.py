664affa164a84a41cc7f85be943ec853ca131a9c
Michelle Senar Dressler <senarmichelle@gmail.com>
2023-02-20 07:45:43 -0800

    Fix verb urls and template

--- src/verbs/urls.py
+++ src/verbs/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     re_path(r"^$", views.home, name="home"),
-    re_path(r"^enfr", views.randomizer, name="randomizer"),
+    re_path(r"^randomizer", views.randomizer, name="randomizer"),
     re_path(r"^about", views.about, name="about"),
     re_path(r"^conjugation/(?P<verb_pk>\d+)/$", views.conjugation, name="conjugation"),
     re_path(r"^memorize", views.memorize, name="memorize"),
